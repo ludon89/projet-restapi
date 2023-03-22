589fc7961f7d4aaf4fc3e77bff8fa20a76f74f71
LuDon89 <daudonlucas@gmail.com>
2023-03-22 11:40:20 +0100

    ajout HearingAidFactory

---
+++ database/factories/HearingAidFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\HearingAid>
+ */
+class HearingAidFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "name" => fake()->words(2, true),
+            "refnumber" => fake()->numberBetween(1000, 9999),
+            "price" => fake()->numberBetween(1000, 2000),
+            "color_id" => fake()->numberBetween(1, 5),
+        ];
+    }
+}
